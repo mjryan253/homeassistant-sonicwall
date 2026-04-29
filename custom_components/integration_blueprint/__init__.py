f7b90e0e71849a65ebc3183bb80e5d35b6fbe397
Matthew Ryan <mjryan1@gmail.com>
2026-04-28 20:10:16 -0400

    chore: resync fork to ludeeus/integration_blueprint upstream

--- custom_components/integration_blueprint/__init__.py
+++ custom_components/integration_blueprint/__init__.py
@@ -1,18 +1,28 @@
-"""Custom integration to integrate integration_blueprint with Home Assistant.
+"""
+Custom integration to integrate integration_blueprint with Home Assistant.
 
 For more details about this integration, please refer to
 https://github.com/ludeeus/integration_blueprint
 """
+
 from __future__ import annotations
 
-from homeassistant.config_entries import ConfigEntry
+from datetime import timedelta
+from typing import TYPE_CHECKING
+
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
-from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.loader import async_get_loaded_integration
 
 from .api import IntegrationBlueprintApiClient
-from .const import DOMAIN
+from .const import DOMAIN, LOGGER
 from .coordinator import BlueprintDataUpdateCoordinator
+from .data import IntegrationBlueprintData
+
+if TYPE_CHECKING:
+    from homeassistant.core import HomeAssistant
+
+    from .data import IntegrationBlueprintConfigEntry
 
 PLATFORMS: list[Platform] = [
     Platform.SENSOR,
@@ -22,17 +32,27 @@ PLATFORMS: list[Platform] = [
 
 
 # https://developers.home-assistant.io/docs/config_entries_index/#setting-up-an-entry
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: IntegrationBlueprintConfigEntry,
+) -> bool:
     """Set up this integration using UI."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator = BlueprintDataUpdateCoordinator(
+    coordinator = BlueprintDataUpdateCoordinator(
         hass=hass,
+        logger=LOGGER,
+        name=DOMAIN,
+        update_interval=timedelta(hours=1),
+    )
+    entry.runtime_data = IntegrationBlueprintData(
         client=IntegrationBlueprintApiClient(
             username=entry.data[CONF_USERNAME],
             password=entry.data[CONF_PASSWORD],
             session=async_get_clientsession(hass),
         ),
+        integration=async_get_loaded_integration(hass, entry.domain),
+        coordinator=coordinator,
     )
+
     # https://developers.home-assistant.io/docs/integration_fetching_data#coordinated-single-api-poll-for-data-for-all-entities
     await coordinator.async_config_entry_first_refresh()
 
@@ -42,14 +62,17 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: IntegrationBlueprintConfigEntry,
+) -> bool:
     """Handle removal of an entry."""
-    if unloaded := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unloaded
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
 
-async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+async def async_reload_entry(
+    hass: HomeAssistant,
+    entry: IntegrationBlueprintConfigEntry,
+) -> None:
     """Reload config entry."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
+    await hass.config_entries.async_reload(entry.entry_id)
--- custom_components/integration_blueprint/config_flow.py
+++ custom_components/integration_blueprint/config_flow.py
@@ -1,4 +1,5 @@
 """Adds config flow for Blueprint."""
+
 from __future__ import annotations
 
 import voluptuous as vol
@@ -6,6 +7,8 @@ from homeassistant import config_entries
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.helpers import selector
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
+from homeassistant.loader import async_get_loaded_integration
+from slugify import slugify
 
 from .api import (
     IntegrationBlueprintApiClient,
@@ -24,7 +27,7 @@ class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(
         self,
         user_input: dict | None = None,
-    ) -> config_entries.FlowResult:
+    ) -> config_entries.ConfigFlowResult:
         """Handle a flow initialized by the user."""
         _errors = {}
         if user_input is not None:
@@ -43,29 +46,44 @@ class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 LOGGER.exception(exception)
                 _errors["base"] = "unknown"
             else:
+                await self.async_set_unique_id(
+                    ## Do NOT use this in production code
+                    ## The unique_id should never be something that can change
+                    ## https://developers.home-assistant.io/docs/config_entries_config_flow_handler#unique-ids
+                    unique_id=slugify(user_input[CONF_USERNAME])
+                )
+                self._abort_if_unique_id_configured()
                 return self.async_create_entry(
                     title=user_input[CONF_USERNAME],
                     data=user_input,
                 )
 
+        integration = async_get_loaded_integration(self.hass, DOMAIN)
+        assert integration.documentation is not None, (  # noqa: S101
+            "Integration documentation URL is not set in manifest.json"
+        )
+
         return self.async_show_form(
             step_id="user",
+            description_placeholders={
+                "documentation_url": integration.documentation,
+            },
             data_schema=vol.Schema(
                 {
                     vol.Required(
                         CONF_USERNAME,
-                        default=(user_input or {}).get(CONF_USERNAME),
+                        default=(user_input or {}).get(CONF_USERNAME, vol.UNDEFINED),
                     ): selector.TextSelector(
                         selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.TEXT
+                            type=selector.TextSelectorType.TEXT,
                         ),
                     ),
                     vol.Required(CONF_PASSWORD): selector.TextSelector(
                         selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.PASSWORD
+                            type=selector.TextSelectorType.PASSWORD,
                         ),
                     ),
-                }
+                },
             ),
             errors=_errors,
         )
